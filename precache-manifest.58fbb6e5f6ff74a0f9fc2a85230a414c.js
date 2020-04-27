d5bfbfa98b3b7ca3f7b85f1f022f68db9ef41193
David <drizzzydave@gmail.com>
2020-04-27 06:51:51 +0100

    Updates

---
+++ precache-manifest.58fbb6e5f6ff74a0f9fc2a85230a414c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2609d61dc8f5b98b155fc650634954b",
+    "url": "/covid-19update/index.html"
+  },
+  {
+    "revision": "ffda9e085e661995c495",
+    "url": "/covid-19update/static/css/main.9ffb462e.chunk.css"
+  },
+  {
+    "revision": "24c86e8ef750b45acf7b",
+    "url": "/covid-19update/static/js/2.aaf0cc2d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19update/static/js/2.aaf0cc2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffda9e085e661995c495",
+    "url": "/covid-19update/static/js/main.ec055537.chunk.js"
+  },
+  {
+    "revision": "8f30e2796e18f3afd068",
+    "url": "/covid-19update/static/js/runtime-main.f718fcc2.js"
+  },
+  {
+    "revision": "feaa946e1a5c7ba46863739a2f352f2b",
+    "url": "/covid-19update/static/media/covid198.feaa946e.png"
+  }
+]);
